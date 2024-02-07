a89a947d633871740be41d6c92b5c19988275b90
aduahimma23 <aduahimma18@gmail.com>
2024-02-07 23:04:55 +0530

    update

--- mainapp/models.py
+++ mainapp/models.py
@@ -5,47 +5,72 @@ class Portfolio(models.Model):
     image = models.ImageField(upload_to='portfolio_images/', blank=True)
     title = models.CharField(max_length=100, default="Mr", blank=False)
     name = models.CharField(max_length=50, default="Tachsol", blank=False)
-    position = models.CharField(max_length=50, default="", blank=True)
-    email = models.EmailField(default="tachsol.km@gmail.com")
+    email = models.EmailField(default="tachsol.km@gmail.com", blank=False)
     content = models.TextField(max_length=1500, default="Tachsol Consult", blank=False)
+    phone_number = models.CharField(max_length=15, default="+1234567890", blank=False, help_text="Enter in the format: +1234567890")
+    work_experience = models.TextField(max_length=1000, blank=False, default='legal practitioner')
+    educational_background = models.TextField(max_length=500, default='Write something about your educational background')
+
+
+class EmploymentRecord(models.Model):
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE)
+    year = models.DateField(auto_now=False)
+    position = models.CharField(max_length=50, default="", blank=True)
+    duties = models.TextField(max_length=500, default='Overall management')
+
+
 
     def __str__(self):
-        return f'{self.title}. {self.name}'
+        return f'{self.portfolio.title}. {self.portfolio.name}'
 
     def clean(self):
-        if not self.email:
+        if not self.portfolio.email:
             raise ValidationError("Email field cannot be empty")
 
+        if not self.portfolio.phone_number.startswith('+'):
+            raise ValidationError("Phone number must start with a plus sign (+)")
+        
+
 class Clients(models.Model):
-    clients_image = models.ImageField(upload_to='clients_image/', blank=False)
-    clients_name = models.CharField(max_length=500, default="@Tachsol Consult LTD", blank=False)
+    image = models.ImageField(upload_to='clients_image/', blank=False)
+    name = models.CharField(max_length=500, default="@Tachsol Consult LTD", blank=False)
     location = models.CharField(max_length=50, default="Accra", blank=False)
     description = models.CharField(max_length=100, default="Tachsol", blank=False)
-    client_contact = models.IntegerField(default=233208157526)
-    url_link = models.URLField(default="http://tachsol.com")
+    contact = models.CharField(max_length=15, default="+233208157526", blank=False, help_text="Enter in the format: +1234567890")
+    website_link = models.URLField(default="http://tachsol.com", blank=True)
+    email = models.EmailField(default="info@tachsol.com", blank=False)
+    sector_of_work = models.CharField(max_length=100, default='Banking')
 
     def __str__(self):
-        return self.clients_name
+        return self.name
 
     def clean(self):
-        if not self.client_contact:
-            raise ValidationError("Client contact field cannot be empty")
+        if not self.contact.startswith('+'):
+            raise ValidationError("Contact number must start with a plus sign (+)")
+
 
 class TeamMembers(models.Model):
     image = models.ImageField()
     name = models.CharField(max_length=100, default="Tachsol")
     position = models.CharField(max_length=50, default="Founder")
-    contact = models.IntegerField(default=233208157526)
+    email = models.EmailField(default="info@tachsol.com")
+
+    def __str__(self):
+        return f'{self.name} {self.email}'
+
+
+class ScopeofOpearation(models.Model):
+    name = models.CharField(max_length=100, blank=False, default='Human Resource Development, Training, and Facilitation')
+    description = models.TextField(max_length=200, blank=True, default='Human Resource Development')
 
     def __str__(self):
         return self.name
+
 
 class Services(models.Model):
-    name = models.CharField(max_length=100, default='Tachsol', blank=False)
-    updated_by = models.CharField(max_length=50, default="Mr. Offei", blank=False)
-    service_image = models.ImageField(upload_to='Service_images/', default='static/images/service/default_image.png', blank=False)
-    content = models.TextField(max_length=1500, default="Enter the text here", blank=False)
-    description = models.CharField(max_length=100, default="We serve you better", blank=False)
+    scope = models.ForeignKey(ScopeofOpearation, on_delete=models.CASCADE)
+    activity = models.CharField(max_length=255, blank=False, default='Training Needs Assessment')
+    description = models.TextField(max_length=255, blank=True, default='Short note about the activity')
 
     def __str__(self):
-        return self.name
+        return self.activity
